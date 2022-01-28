0d1e9354351540019144be3833d91074056909ef
Charlie Huang <charliehuang@Charlies-MacBook-Pro.local>
2022-01-27 19:46:58 -0800

    before presentation

--- client/components/PriorWorkout.js
+++ client/components/PriorWorkout.js
@@ -4,7 +4,7 @@ import UpdateButton from './UpdateButton';
 const PriorWorkout = props => {
   const workouts = props.workouts.map((el, i) => {
     const { _id, date, workout, weight, reps, notes } = el
-    const props = { _id, date, workout, weight, reps, notes }
+    const attr = { _id, date, workout, weight, reps, notes }
     return(
       <div className='workoutResult' key={i}>
         <b>Date: </b>{date.split('T')[0]}<br></br>
@@ -13,7 +13,7 @@ const PriorWorkout = props => {
         <b>Reps: </b>{reps}<br></br>
         <b>Notes: </b>{notes}<br></br>
         <button className='deleteButton' onClick={() => props.handleClick(_id)}>Delete</button>
-        <UpdateButton {...props} />
+        <UpdateButton {...attr} />
       </div>
     )
   });
